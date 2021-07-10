ae01312be602b3886937dcfb30b38b321198afdc
jiwon79 <19-079@ksa.hs.kr>
2021-07-11 01:47:32 +0900

    feat: Add Box drag drop function

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".area");
+const box = document.querySelector(".area__box");
+
+const {width: containerWidth, height: containerHeight} = container.getBoundingClientRect();
+const {width: boxWidth, height: boxHeight} = box.getBoundingClientRect();
+
+let isDragging = null;
+let originLeft = null;
+let originTop = null;
+let originX = null;
+let originY = null;
+
+box.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    originX = e.clientX;
+    originY = e.clientY;
+    originLeft = box.offsetLeft;
+    orginTop = box.offsetTop;
+});
+
+document.addEventListener("mouseup", (e) => {
+    isDragging = false;
+});
+
+document.addEventListener("mousemove", (e) => {
+    if(isDragging) {
+        const diffX = e.clientX - originX;
+        const diffY = e.clientY - originY;
+        const endOfXPoint = containerWidth - boxWidth;
+        const endOfYPoint = containerHeight - boxHeight;
+        box.style.left = `${Math.min(Math.max(0, originLeft+diffX), endOfXPoint)}px`;
+        box.style.top = `${Math.min(Math.max(0, originTop+diffY), endOfYPoint)}px`;
+    }
+});
