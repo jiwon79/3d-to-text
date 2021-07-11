c0fc681891bf82f6f338d5939cba4a8666e026a6
jiwon79 <19-079@ksa.hs.kr>
2021-07-12 00:20:25 +0900

    feat: Add dynamic table

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const container = document.querySelector(".area");
+const display = document.querySelector(".display");
 
 let isDragging = null;
 let originX = null;
 let originY = null;
+
+const WIDTH = 20;
+const HEIGHT = 20;
+
+for (var i=0; i<WIDTH; i++) {
+    var th = document.createElement('th');
+    display.appendChild(th);
+    for (var j=0; j<HEIGHT; j++) {
+        var tr = document.createElement('tr');
+        th.appendChild(tr);
+        tr.innerHTML = '0';
+    }
+}
+
 
 container.addEventListener("mousedown", (e) => {
     isDragging = true;
