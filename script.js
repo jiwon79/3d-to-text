f7b87b3a7f36f792ae5d90668bd1bd6c1d823090
jiwon79 <19-079@ksa.hs.kr>
2021-07-13 19:14:35 +0900

    feat: Add get luminance function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const M = Math;
 const container = document.querySelector(".area");
 const display = document.querySelector(".display");
 
@@ -8,6 +9,7 @@ let originY = null;
 const WIDTH = 40;
 const HEIGHT = 30;
 
+// Create WIDTH, HEIGHT size table
 for (var i=0; i<HEIGHT; i++) {
     var tr = document.createElement('tr');
     display.appendChild(tr);
@@ -18,7 +20,7 @@ for (var i=0; i<HEIGHT; i++) {
     }
 }
 
-
+// drag and drop mouse event
 container.addEventListener("mousedown", (e) => {
     isDragging = true;
     originX = e.clientX;
@@ -37,3 +39,61 @@ document.addEventListener("mousemove", (e) => {
         document.querySelector('.text p:nth-child(2)').innerHTML = String(diffY);
     }
 });
+
+
+const THETA_NUM = 10;
+const PHI_NUM = 10;
+const a = 5;
+const b = 12;
+const LIGHT = [0,1,-1];
+const CHAR = ['.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@']
+
+// Calculate vector magnitude
+function magnitude(vec) {
+    result = 0;
+    for (var i=0; i<vec.length; i++) {
+        result += vec[i]*vec[i]
+    }
+
+    return M.sqrt(result)
+}
+
+// Calculate unit vector
+function unit(vec) {
+    var mag = magnitude(vec);
+    var result = [];
+    for (var i=0; i<vec.length; i++) {
+        result.push(vec[i]/mag);
+    }
+
+    return result;
+}
+
+function dotProduct(vec1, vec2) {
+    if (vec1.length != vec2.length) {
+        return false;
+    }
+
+    var result = 0;
+    for (var k=0; k<vec1.length; k++) {
+        result += vec1[k]*vec2[k]
+    }
+    return result
+}
+
+for (var i=0; i<THETA_NUM; i++) {
+    for (var j=0; j<PHI_NUM; j++) {
+        var theta = 2*M.PI*i/THETA_NUM;
+        var phi = 2*M.PI*j/PHI_NUM;
+
+        var r = [
+            M.cos(phi)*(a*M.cos(theta)+b),
+            M.sin(phi)*(a*M.cos(theta)+b),
+            a*M.sin(theta)
+        ];
+        var normal = unit([M.cos(theta), M.sin(theta), M.tan(phi)]);
+
+        var luminance = dotProduct(normal, [0,1,-1])*8;
+        console.log(luminance);
+    }
+}
