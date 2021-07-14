91418423f934524300ae0ebead173ce86cca8044
jiwon79 <19-079@ksa.hs.kr>
2021-07-14 22:07:54 +0900

    feat: Settle overlap points by z axis

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let originX = null;
 let originY = null;
 
 const WIDTH = 40;
-const HEIGHT = 30;
+const HEIGHT = 40;
 
 // Create WIDTH, HEIGHT size table
 for (var i=0; i<HEIGHT; i++) {
@@ -16,7 +16,7 @@ for (var i=0; i<HEIGHT; i++) {
     for (var j=0; j<WIDTH; j++) {
         var td = document.createElement('td');
         tr.appendChild(td);
-        td.innerHTML = '#';
+        td.innerHTML = ' ';
     }
 }
 
@@ -41,11 +41,11 @@ document.addEventListener("mousemove", (e) => {
 });
 
 
-const THETA_NUM = 10;
-const PHI_NUM = 10;
+const THETA_NUM = 100;
+const PHI_NUM = 100;
 const a = 5;
 const b = 12;
-const LIGHT = [0,1,-1];
+const LIGHT = [0,-1,1];
 const CHAR = ['.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@']
 
 // Calculate vector magnitude
@@ -69,6 +69,7 @@ function unit(vec) {
     return result;
 }
 
+// Calculate dot product
 function dotProduct(vec1, vec2) {
     if (vec1.length != vec2.length) {
         return false;
@@ -81,19 +82,49 @@ function dotProduct(vec1, vec2) {
     return result
 }
 
+// Create 2 dimention array
+function create2DArray(rows, columns) {
+    var arr = new Array(rows);
+    for (var i = 0; i < rows; i++) {
+        arr[i] = new Array(columns);
+        arr[i].fill(-1);
+    }
+    return arr;
+}
+
+zArray = create2DArray(WIDTH, HEIGHT);
+
 for (var i=0; i<THETA_NUM; i++) {
     for (var j=0; j<PHI_NUM; j++) {
         var theta = 2*M.PI*i/THETA_NUM;
         var phi = 2*M.PI*j/PHI_NUM;
-
+        
         var r = [
             M.cos(phi)*(a*M.cos(theta)+b),
             M.sin(phi)*(a*M.cos(theta)+b),
             a*M.sin(theta)
         ];
-        var normal = unit([M.cos(theta), M.sin(theta), M.tan(phi)]);
 
-        var luminance = dotProduct(normal, [0,1,-1])*8;
-        console.log(luminance);
+        var normal = unit([
+            M.cos(theta)*M.cos(phi), 
+            M.cos(theta)*M.sin(phi), 
+            M.sin(theta)
+        ]);
+        var luminance = M.floor(dotProduct(normal, LIGHT)*8);
+
+        r = [
+            M.floor(r[0]+WIDTH/2),
+            M.floor(r[1] + HEIGHT/2),
+            r[2] = M.floor(r[2])
+        ];
+
+        if(r[0]==M.floor(WIDTH/2+1) && r[1]==8){
+            console.log(normal,luminance,theta,unit([M.cos(theta), M.sin(theta), M.tan(phi)]),r)
+        }
+        if (0<r[0]<WIDTH && 0<r[1]<HEIGHT && luminance>=0 && zArray[r[0]][r[1]]<r[2]) {
+            zArray[r[0]][r[1]] = r[2];
+            document.querySelector('table tr:nth-child('+String(r[0])+') td:nth-child('+String([r[1]])+')').innerHTML = CHAR[luminance];
+        }
+        
     }
 }
